7d741a26c37dd4f1f0261beafd0e4426b20f5a17
hamzzah-10 <126707087+hamzzah-10@users.noreply.github.com>
2023-11-09 13:08:13 +0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+
+CWC92 = input("Cricket Worldcup: ")
+CWC92_Captain = input("Cricket Worldcup Captain: ")
+T20WC = input("T20 Worldcup: ")
+T20WC_Captain = input("T20 Worldcup Captain: ")
+CT17 = input("Champions Trophy: ")
+CT_Captain = input("Champions Trophy Captain: ")
+
+madlib = f"Pakistan won Cricket World Cup in {CWC92} under the captaincy of Great {CWC92_Captain}. \
+           Pakistan won T20 World Cup in {T20WC} under captaincy of {T20WC_Captain}. \
+           Pakistan won Champions Trophy in {CT17} under captaincy of {CT_Captain}."
+
+print(madlib)
